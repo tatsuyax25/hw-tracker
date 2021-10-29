b47d9aae0d4e7e1f78f85a8ac9ed4d5c7243c710
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-28 21:05:19 -0700

    add category dropdown to todos

--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+HW = (
+    ('D', 'Deliverable'),
+    ('OL', 'Optional Lab'),
+    ('OC', 'Outcomes'),
+    ('PR', 'Project'),
+    ('RL', 'Rewatch Lecture'),
+    ('T', 'Tutorial'),
+    ('0T', 'Other'),    
+)
 
 class Todo(models.Model):
-    todo = models.CharField(max_length=100)
-    # category = models. (should have choices)
+    todo = models.CharField('name', max_length=100)
+    category = models.CharField(
+        max_length=2,
+        choices = HW,
+        default=HW[6][0]
+    ) 
     description = models.TextField(max_length=250)
     date = models.CharField('due date', max_length=30)
     time = models.CharField(max_length=12)
@@ -24,7 +37,7 @@ class Todo(models.Model):
 
 class Note(models.Model):
     note = models.CharField(max_length=100)
-    content = models.TextField(max_length=500)
+    content = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
