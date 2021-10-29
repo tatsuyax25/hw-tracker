bc879a86df52d8920732aa73883a6283b9258317
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-28 21:52:53 -0700

    fix all heroku issues

--- main_app/models.py
+++ main_app/models.py
@@ -19,12 +19,11 @@ class Todo(models.Model):
     category = models.CharField(
         max_length=2,
         choices = HW,
-        default=HW[6][0]
+        default=HW[0][0]
     ) 
     description = models.TextField(max_length=250)
     date = models.CharField('due date', max_length=30)
     time = models.CharField(max_length=12)
-    test = models.CharField(max_length=100)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
