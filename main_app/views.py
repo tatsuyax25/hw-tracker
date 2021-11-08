98ea3fda2ed38cb9a7a052b3cf79effc170a9925
Amar Panjwani <amar.panjwani@gmail.com>
2021-11-08 12:16:10 -0800

    revert view index permissions to default after testing

--- main_app/views.py
+++ main_app/views.py
@@ -24,8 +24,7 @@ def about(request):
 
 @login_required
 def todos_index(request):
-    #todos = Todo.objects.filter(user=request.user)
-    todos = Todo.objects.filter()
+    todos = Todo.objects.filter(user=request.user)
     return render(request, 'todos/index.html', {'todos': todos})
 
 
@@ -57,8 +56,7 @@ class TodoDelete(LoginRequiredMixin, DeleteView):
 
 @login_required
 def notes_index(request):
-    #notes = Note.objects.filter(user=request.user)
-    notes = Note.objects.filter()
+    notes = Note.objects.filter(user=request.user)
     return render(request, 'notes/index.html', { 'notes': notes })
 @login_required
 def notes_detail(request, note_id):
